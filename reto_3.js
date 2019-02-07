976d509cbafc912688d03b281d80169fbdc05c2e
CESARSOAD07 <42721388+CESARSOAD07@users.noreply.github.com>
2019-02-07 09:14:48 -0600

    Create reto_3.js

---
+++ reto_3.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(255);
+  stroke(255,0,0);
+  ellipse(200,200,300,300);
+  strokeWeight(20);
+  line(95,95,300,300);
+  strokeWeight(20);
+  
+}
